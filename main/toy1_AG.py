8967992d234872c95cd0d50713e921cea911f706
karyouben <karyouben@alum.us.es>
2024-06-06 16:57:33 +0200

    fix ruta

--- main/toy1_AG.py
+++ main/toy1_AG.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 from sklearn.metrics import root_mean_squared_error, r2_score
 import time
-from main.AG_class import AG
+from AG_class import AG
 
 
 # Nombre generico del dataset
 nombre_dataset = 'toy1'
 
-data_folder = 'data'
+data_folder = r'C:\Users\willi\workspace-VisualStudio\AG_IA_2324\main\data'
 
 nombre_dataset_train = os.path.join(data_folder, nombre_dataset + "_train.csv")
 nombre_dataset_val = os.path.join(data_folder, nombre_dataset + "_val.csv")
