25080ad72f99357aff324da38d9811752d0c0034
karyouben <karyouben@alum.us.es>
2024-06-09 23:45:28 +0200

    añadido toy1

--- main/AG_experiment_parallelized.py
+++ main/AG_experiment_parallelized.py
@@ -59,7 +59,7 @@ def main():
         'crossover_rate': [0.6, 0.72, 0.85]
     }
     data_folder = "./data"
-    dataset_name = 'synt1'
+    dataset_name = 'toy1'
     results_folder = "./results"
 
     if not os.path.exists(results_folder):
